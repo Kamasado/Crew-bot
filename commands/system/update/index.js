a4c92d002dff9e81ff7c149d0e3a748966002d0c
Gabriel Lobo <gabrieleduardolobog@gmail.com>
2019-01-12 07:20:03 -0400

    add update command

---
+++ commands/system/update/index.js
@@ -0,0 +1,31 @@
+const shell = require("shelljs");
+const fs = require("fs");
+
+const config = JSON.parse(fs.readFileSync(__basedir + "/bot_config.json"));
+const helpers = require(__basedir + "/commands/helpers");
+const user = require(__basedir + "/userModel");
+const user = require(__basedir + "/alertModel");
+
+const errmsg = helpers.error(
+  "Sintaxis incorrecta",
+  `Uso: **${config.prefix}system update**`
+);
+
+function valid(arg) {
+  if (arg.length !== 0) {
+    return false;
+  }
+  return true;
+}
+
+module.exports = (arg, msg) => {
+  if (!valid(arg)) {
+    msg.channel.send(errmsg);
+    return;
+  }
+
+  success = helpers.success("Sistema", "Actualizando archivos...");
+
+  shell.exec("git pull");
+  msg.channel.send(success).then(() => process.exit());
+};
