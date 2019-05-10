0a0d75fea84d862a3d1f6a293a0fc05eb3e26a51
Hiro <20100988+Kamasado@users.noreply.github.com>
2019-05-09 21:25:14 -0400

    startpoint fix

--- startpoint.sh
+++ startpoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # discord server
-screen -dm -S 'discord' yarn start
+./start_discord.sh -D
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start my_first_process: $status"
+  exit $status
+fi
+
 # http server
 node http.js
